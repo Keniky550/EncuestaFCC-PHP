fe6bd2765f90dd349e3e6c4d20b252f8d2d5d2e7
Keniky550 <126686075+Keniky550@users.noreply.github.com>
2023-08-13 22:48:25 -0500

    carpeta css

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/ingreso.css">
+    <title>LoginFCC</title>
+</head>
+<body>
+    <div class="register">
+        <h2>Encuesta FCC</h2>
+        <form action="registro.html">
+            <div class="group">
+                <div class="log">
+                    <label id="login1" for="cod">Código:</label>
+                    <input type="text" id="cod" name="cod" size="8">
+                </div>
+                <div class="log">
+                    <label id="login2" for="dni">DNI:</label>
+                    <input type="text" id="dni" name="dni" size="8">
+                </div>
+            </div>
+            <input class="log" type="submit" value="INGRESAR">
+        </form>
+    </div>
+</body>
+</html>
