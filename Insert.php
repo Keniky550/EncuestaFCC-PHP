2225644dcbf06091550d6e33bf96ae7a1ef9e3de
Keniky550 <ken.jav550@gmail.com>
2023-08-17 12:18:15 -0500

    insertó

--- Insert.php
+++ Insert.php
@@ -2,16 +2,19 @@
     require("db.php");
     $conexion = conection();
 
-    $archivo = fopen("./datos/SCHOOL.csv", "r");
+    $archivo = fopen("./datos/DOC_TYPE.csv", "r");
     $con = 0;
 
-    while(($datos  = fgetcsv($archivo, 0, ","))== true){
-        $con++;
-
-        $sql = "INSERT INTO school VALUES ('','$datos[0]'";
-        
-        if($conexion->query($sql) === TRUE){
-            echo "SE INSERTÓ CORRECTAMENTE";
+    try {
+        $conexion -> beginTransaction();
+        while(($datos  = fgetcsv($archivo, 0, ","))== true){
+            $con++;
+            $conexion -> exec("INSERT INTO school VALUES ('','$datos[0]')"); 
         }
+        $conexion -> commit();
+    } catch (PDOException $e) {
+        echo "ERROR: " .$e->getMessage();
     }
+    
+    fclose($archivo);
 ?>
