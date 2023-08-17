c4e00342dc5750a3a69217300dc4a76f979d27fe
Keniky550 <ken.jav550@gmail.com>
2023-08-16 23:50:40 -0500

    in school

--- Insert.php
+++ Insert.php
@@ -2,13 +2,13 @@
     require("db.php");
     $conexion = conection();
 
-    $archivo = fopen("./datos/STUDENT.csv", "r");
+    $archivo = fopen("./datos/SCHOOL.csv", "r");
     $con = 0;
 
     while(($datos  = fgetcsv($archivo, 0, ","))== true){
         $con++;
 
-        $sql = "INSERT INTO STUDENT VALUES ('','$dato[0]','','$dato[1]','$dato[2]','$dato[3]','$dato[4]'),''";
+        $sql = "INSERT INTO school VALUES ('','$datos[0]'";
         
         if($conexion->query($sql) === TRUE){
             echo "SE INSERTÓ CORRECTAMENTE";
