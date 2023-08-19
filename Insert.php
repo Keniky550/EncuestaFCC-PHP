abe5fabff520c9c459e969688ebab51a18d75d85
Keniky550 <ken.jav550@gmail.com>
2023-08-19 15:17:41 -0500

    posible insert

--- Insert.php
+++ Insert.php
@@ -1,20 +1,82 @@
 <?php
-    require("conn.php");
-    $conexion = conection();
-
-    $archivo = fopen("./datos/DOC_TYPE.csv", "r");
-    $con = 0;
-
-    try {
-        $conexion -> beginTransaction();
-        while(($datos  = fgetcsv($archivo, 0, ","))== true){
-            $con++;
-            $conexion -> exec("INSERT INTO school VALUES ('','$datos[0]')"); 
-        }
-        $conexion -> commit();
-    } catch (PDOException $e) {
-        echo "ERROR: " .$e->getMessage();
-    }
+    require_once("conn.php");
     
-    fclose($archivo);
+
+    function insertSchool(){
+        $kenneth = new KEN();
+        $archivo = fopen("./datos/SCHOOL.csv", "r");
+        $conexion = $kenneth->conection();
+        try {
+            $conexion -> beginTransaction();
+            while(($datos  = fgetcsv($archivo, 0, ","))== true){
+                $conexion -> exec("INSERT INTO school VALUES ('','$datos[0]')"); 
+            }
+            $conexion -> commit();
+        } catch (PDOException $e) {
+            echo "ERROR: " .$e->getMessage();
+        }
+        
+        fclose($archivo);
+    }
+
+    function insertDocType(){
+        $kenneth = new KEN();
+        $archivo = fopen("./datos/DOC_TYPE.csv", "r");
+        $conexion = $kenneth->conection();
+        try {
+            $conexion -> beginTransaction();
+            while(($datos  = fgetcsv($archivo, 0, ","))== true){
+                $conexion -> exec("INSERT INTO doc_student VALUES ('','$datos[0]')"); 
+            }
+            $conexion -> commit();
+        } catch (PDOException $e) {
+            echo "ERROR: " .$e->getMessage();
+        }
+        
+        fclose($archivo);
+    }
+
+    function insertStudent(){
+        $kenneth = new KEN();
+        $archivo = fopen("./datos/STUDENT.csv", "r");
+        $conexion = $kenneth->conection();
+        try {
+            $conexion -> beginTransaction();
+            while(($datos  = fgetcsv($archivo, 0, ","))== true){
+                $conexion -> exec("INSERT INTO student VALUES ('','$datos[0],'$datos[1]','$datos[2]','$datos[3]','$datos[4]','$datos[5]','$datos[6]')"); 
+            }
+            $conexion -> commit();
+        } catch (PDOException $e) {
+            echo "ERROR: " .$e->getMessage();
+        }
+        
+        fclose($archivo);
+    }
+ 
+    function insertTemporal(){
+        $kenneth = new KEN();
+        $archivo = fopen("./datos/TEMPORAL.csv", "r");
+        $conexion = $kenneth->conection();
+        $sql = 'SELECT id_sudent FROM student';
+        //$result = $conexion->query($sql);
+        try {
+            $conexion -> beginTransaction();
+            $dat = $kenneth -> consultar($sql);  
+            foreach ($dat as $val){
+                $id=$val['id_sudent'];
+                while(($datos  = fgetcsv($archivo, 0, ","))== true){
+                    $conexion -> exec("INSERT INTO temporal VALUES ('','$id','$datos[0]','$datos[1]','$datos[2]','$datos[3]','$datos[4]','$datos[5]')"); 
+                }
+            }
+            $conexion -> commit();
+        } catch (PDOException $e) {
+            echo "ERROR: " .$e->getMessage();
+        }
+        
+        fclose($archivo);
+    }
+    insertSchool();
+    insertDocType();
+    insertStudent();
+    insertTemporal();
 ?>
--- conn.php
+++ conn.php
@@ -1,18 +1,33 @@
 <?php
-    function conection(){
-        $conexion = null;
-        $server = 'localhost';
-        $user = 'root';
-        $password = '';
-        $bd = 'encuesta';
-        
-        try {
-            $conexion = new PDO("mysql:host=".$server.";dbname=".$bd, $user, $password);
-            return $conexion;
-        }catch (PDOException $e){
-            echo "sin conexion: " . $e -> getMessage();
-            return null;
+    class KEN{
+        private $conexion;
+        function conection(){
+           
+            $server = 'localhost';
+            $user = 'root';
+            $password = '';
+            $bd = 'encuesta';
+            
+            try {
+                $this->conexion = new PDO("mysql:host=".$server.";dbname=".$bd, $user, $password);
+                return $this->conexion;
+            }catch (PDOException $e){
+                echo "sin conexion: " . $e -> getMessage();
+                return null;
+            }
+    
         }
-
+    
+        function consultar($sql,$valores = array()) {
+            if($sql !='' && strlen($sql)>0){
+                $consulta = $this->conexion->prepare($sql);
+                $consulta->execute();
+                $resultados=$consulta->fetchAll(PDO::FETCH_ASSOC);
+                
+                return $resultados;
+            }else{
+                return 0;
+            }
+        }
     }
 ?>
